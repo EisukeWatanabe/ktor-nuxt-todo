11e2db107e3098f9737c921231e053520035f4a8
渡邉英資 <awatanabe@fminc.xsrv.jp>
2023-04-09 09:41:39 +0900

    create ktor project

---
+++ src/main/kotlin/com/todo/example/Application.kt
@@ -0,0 +1,15 @@
+package com.todo.example
+
+import io.ktor.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.tomcat.*
+import com.todo.example.plugins.*
+
+fun main() {
+    embeddedServer(Tomcat, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
